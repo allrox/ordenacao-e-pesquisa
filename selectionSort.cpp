75f6408af2ea37a8d0e2ae287a7d03690a48ed28
allrox <alexandre@allrox.com.br>
2021-09-15 17:21:13 -0300

    Pequenos ajustes no selectionSort.cpp

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -5,8 +5,8 @@
 
 using namespace std;
 
-//Aqui são chamados os protótipos de função,
-//Declarados após a função Main
+//Aqui sÃ£o chamados os protÃ³tipos de funÃ§Ã£o,
+//Declarados apÃ³s a funÃ§Ã£o Main
 void selectionSort(int vet[], int tam);
 void imprimirVetor(int vet[], int tam);
 
@@ -14,29 +14,29 @@ int main(){
 
 	setlocale(LC_ALL, "Portuguese");
 	
-	//Declaração das variáveis do vetor 
-	//e do contador para as iterações
+	//DeclaraÃ§Ã£o das variÃ¡veis do vetor 
+	//e do contador para as iteraÃ§Ãµes
 	int i, vetor[5];
 		
-	//Loop para inserção dos valores do vetor
+	//Loop para inserÃ§Ã£o dos valores do vetor
 	for(i=0;i<5;i++){
-		cout<<"Informe o "<<i+1<<"º termo: ";
+		cout<<"Informe o "<<i+1<<"o termo: ";
 		cin>>vetor[i];
 	}
 	
 	
-	//Chamada para a exibição do vetor in-natura
-	cout<<"\nSeu vetor não ordenado com os dados inseridos: ";
+	//Chamada para a exibiÃ§Ã£o do vetor in-natura
+	cout<<"\nSeu vetor nÃ£o ordenado com os dados inseridos: ";
 	cout<<"{ ";
 	imprimirVetor(vetor,5);
 	cout<<" }";
 	cout<<"\n\n";
 	
-	//Chamada da função que reordena e exibe os elementos
+	//Chamada da funÃ§Ã£o que reordena e exibe os elementos
 	selectionSort(vetor,5);
 	
-	//Chama a função que exibe o vetor com os elementos reordenados
-	cout<<"\nSeu vetor ordenado utilizando o método Selection Sort: ";
+	//Chama a funÃ§Ã£o que exibe o vetor com os elementos reordenados
+	cout<<"\nSeu vetor ordenado utilizando o mÃ©todo Selection Sort: ";
 	cout<<"{ ";
 	imprimirVetor(vetor, 5);
 	cout<<" }";
@@ -52,14 +52,14 @@ void selectionSort(int vet[], int tam){
 	int indexMin, i, j;
 	
 	//A partir de um contador "i" zerado, 
-	//enquanto menor que a variável "tam", incremente 1
+	//enquanto menor que a variÃ¡vel "tam", incremente 1
 	for(i=0;i<tam-1;i++){
 		
-		//Declaração da variável que receberá
-		//o menor valor atual admissível para o índice
+		//DeclaraÃ§Ã£o da variÃ¡vel que receberÃ¡
+		//o menor valor atual admissÃ­vel para o Ã­ndice
 		indexMin=i;
 		
-		//A partir da varíavel "j" maior que zero, 
+		//A partir da variavel "j" maior que zero, 
 		//enquanto "j" menor que "tam", incremente 1
 		for(j=i+1;j<tam;j++){
 			
@@ -70,10 +70,10 @@ void selectionSort(int vet[], int tam){
 			}
 		}
 		
-		//Exibe o elemento "i" do vetor seguido do elemento atribuído a "indexMin
+		//Exibe o elemento "i" do vetor seguido do elemento atribuÃ­do a "indexMin
 		cout<<"Troca: "<<vet[i]<<" <= "<<vet[indexMin]<<endl;
 		
-		//Troca de posição dos elementos no vetor a partir da comparação
+		//Troca de posiÃ§Ã£o dos elementos no vetor a partir da comparaÃ§Ã£o
 		//Aprofundar...
 		if(indexMin != i) {
 			int temp = vet[indexMin];
@@ -83,14 +83,14 @@ void selectionSort(int vet[], int tam){
 	}
 }
 
-//Função que imprime o vetor
+//FunÃ§Ã£o que imprime o vetor
 void imprimirVetor (int vet[],int tam){
 	int i;
 	
-	//Saída do primeiro elemento
+	//SaÃ­da do primeiro elemento
 	cout<<vet[0];
 	
-	//Loop para exibição dos demais elementos do vetor
+	//Loop para exibiÃ§Ã£o dos demais elementos do vetor
 	for(i=1;i<5;i++){
 		cout<<", "<<vet[i];
 	}
